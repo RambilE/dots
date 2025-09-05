543ec7073d7d3910bbcf22782087e4c8b5549a68
RambilE <rambile@disroot.org>
2025-09-05 17:43:51 +0500

    gsr script improvements

--- global/rambile/dotscripts/gsr-wofi.sh
+++ global/rambile/dotscripts/gsr-wofi.sh
@@ -4,19 +4,20 @@ if [[ $(pidof gpu-screen-recorder-gtk) == "" ]]; then
         exit
 fi
 
-choice=$(printf "Toggle sleep state\nStop recording/replay\nSave replay" | wofi --dmenu --prompt="GSR controls")
+choice=$(printf "Save replay\nStop recording/replay\nToggle record sleep" | wofi --dmenu --prompt="GSR controls" -k /dev/null)
 
 #if [ "$choice" == "Open GSR UI" ]; then
 #        nohup gpu-screen-recorder-gtk &
 
-if [ "$choice" == "Toggle sleep state" ]; then
-        killall -SIGUSR2 gpu-screen-recorder; notify-send "GSR sleep is toggled!" "Idk the state though (and it'll lie in the gui"
+if [ "$choice" == "Toggle record sleep" ]; then
+    killall -SIGUSR2 gpu-screen-recorder; notify-send -t 2000 "GSR sleep is toggled!" "Gui is lying."
 
 elif [ "$choice" == "Stop recording/replay" ]; then
-        killall -SIGINT gpu-screen-recorder
+    killall -SIGINT gpu-screen-recorder
 
 elif [ "$choice" == "Save replay" ]; then
-        killall -SIGUSR1 gpu-screen-recorder; notify-send "Saved replay!" "Should be in ~/Videos"
+    replayDir=$(cat ~/.config/gpu-screen-recorder/config | grep -oP "(replay.save_directory)\K.*")
+    killall -SIGUSR1 gpu-screen-recorder; notify-send -t 2000 "Saved replay!" "Saved in $replayDir"
 
 fi
 
