44c4b02b055b328c16ae41bb9a0e57ad07ab7012
RambilE <rambile@disroot.org>
2025-09-06 21:49:14 +0500

    waybar: scroll on workspaces

--- global/rambile/dotscripts/fullscreencheck.lua
+++ global/rambile/dotscripts/fullscreencheck.lua
@@ -13,28 +13,54 @@ local HIS =   os.getenv("HYPRLAND_INSTANCE_SIGNATURE")
 assert(socket.connect(fd, {family = socket.AF_UNIX, path = ""..XDRD.."/hypr/"..HIS.."/.socket2.sock"}))
 
 local wsCache = {0,0,0,0,0,0,0,0,0,0}
---local ws = 1
---local fsState = 0
+
+-- ty github.com/VanderCat
+function string.split(inputstr, sep)
+  if sep == nil then
+    sep = "%s"
+  end
+  local t = {}
+  for str in string.gmatch(inputstr, "([^"..sep.."]+)") do
+    table.insert(t, str)
+  end
+  return t
+end
+
+function tablelength(T)
+  local count = 0
+  for _ in pairs(T) do count = count + 1 end
+  return count
+end
+
 while true do
     bytes = assert(socket.recv(fd, 1337))
     -- print(bytes)
     for i in string.gmatch(bytes, "%S+") do
-        if string.find(i, "^workspace>>%d$") then -- find workspace that we are switching to
+        if string.find(i, "^workspace>>%d$") then -- find ws that we are moving to
             ws,_ = (string.gsub(i,"workspace>>",""))
+            -- print(ws)
         end
         if string.find(i, "^fullscreen>>%d$") then -- find fullscreen state
             fsState,_ = (string.gsub(i,"fullscreen>>",""))
-            if ws and fsState then
+            if ws and fsState ~= nil then
                 wsCache[ws] = fsState
             end
         end
-        -- if string.find(i, "^movewindow>>*,%d$") then
-        --     nextWs,_ = (string.gsub(i,"movewindow>>",""))
-        --     print(nextWs)
-        -- end
+        if string.find(i, "movewindow>>%g") then -- get ws that we are moving window to
+            mvwindowWs,_ = (string.gsub(i,"movewindow>>",""))
+            mvwindowtWs = string.split(mvwindowWs,",")
+            -- print(mvwindow[2])
+            -- if fsState then
+            --     wsCache[mvwindowWs] = 1
+            --     wsCache[ws] = 0
+            -- end
+        end
     end
-    -- print(ws, fsState, wsCache[ws])
-    -- print(type(ws))
+    -- print("1 2 3 4 5 6 7 8 9 10 ", "table len:" ..tablelength(wsCache))
+    -- for i in pairs(wsCache) do
+    --     io.write(wsCache[i].. " ")
+    -- end
+    -- print("\n")
 
     if wsCache[ws] == "0" or wsCache[ws] == nil then
         print(" ")
